e34464c6a14548306945d1d868443645cc7e795e
PaulaSadko <student-wroclaw@EDU-WSB.local>
2023-01-21 11:54:18 +0100

    Commit uzupenienie

--- lib_os_time.py
+++ lib_os_time.py
@@ -10,9 +10,18 @@ import os
 #standardowa biblioteka, która już jest wgrana
 
 print(os.getcwd())
-#sprawdz current working directory, gdzie jestem
+#sprawdź current working directory, gdzie jestem
 
-from os import *
-#drugi sposob na import wszystkiego * z bliioteki funkcji metod klas
+#from os import *
+#drugi sposób na import wszystkiego * z bliioteki funkcji metod klas
+
+os.chdir('C:\\Users\\vdi-student\\Desktop')
+#zawsze w pythonie dwa \\ dzialaja jak jednen \, w ścieżce html to nie obowiązuje
+
+print(os.getcwd())
+#ponownie sprawdzamy lokalizajcę
+
+os.mkdir('new_folder')
+#tworzenie nowego folderu
 
 
