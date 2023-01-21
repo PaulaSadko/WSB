ca2553fbb4f0d52b59a22ec511e3b2a843982497
PaulaSadko <student-wroclaw@EDU-WSB.local>
2023-01-21 13:08:19 +0100

    Paula

--- the_csv.py
+++ the_csv.py
@@ -1,13 +1,18 @@
 #1. Odczyt pliku csv z zapisem poszczegolnych pol
 
 #with open(path, mode) as plik_csv:
+#lub incaczej można zapisać
+#with open(C:\\Users\\vdi-student\\Desktop\\WSB\\rozliczenie.csv','r') as plik_csv:
 path = 'C:\\Users\\vdi-student\\Desktop\\WSB\\rozliczenie.csv'
 mode = 'r'
 with open(path, mode) as plik_csv:
+    content = plik_csv.readlines()
 
-#lub incaczej mpżna zapisać
-#with open(C:\\Users\\vdi-student\\Desktop\\WSB\\rozliczenie.csv','r') as plik_csv:
-
+print(type(content))
+print(len(content))
+print(content)
+print(content[4])
+#pokaże piątą linię
 
 
 
