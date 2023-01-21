aae824651229388120fd86377dc5f236ad54b89f
PaulaSadko <student-wroclaw@EDU-WSB.local>
2023-01-21 11:22:03 +0100

    Commit pierwszego pliku

---
+++ WSB.py
@@ -0,0 +1,26 @@
+print('Hej')
+#coś wyświetlamy
+
+a = 5
+b = 7
+c = 'Paula'
+
+print(a)
+print('a')
+print(c)
+print('c')
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+#pokazuje jaki typ danych
+
+print(c[2])
+#poaże trzecia literę
+
+print('Trzecia litera to',c[2])
+
+print(a + b)
+print(a * b)
+#działania
